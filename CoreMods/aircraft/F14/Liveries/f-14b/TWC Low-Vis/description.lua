e3ba7250cedbbd2e04ba8c842bd6fec64d39be57
Aleyboy <aleyboy23@hotmail.co.uk>
2021-11-27 21:02:21 +0000

    Mod removal, Mig 28 and F-14

---
+++ CoreMods/aircraft/F14/Liveries/f-14b/TWC Low-Vis/description.lua
@@ -0,0 +1,18 @@
+livery = {
+
+
+--Diffuse  -----------------------------------
+
+{"HB_F14_EXT_01", 0 ,"TWC_F14_EXT_01",false};
+{"HB_F14_EXT_02", 0 ,"TWC_F14_EXT_02",false};
+{"HB_F14_EXT_03", 0 ,"TWC_F14_EXT_03",false};
+{"HB_F14_EXT_04", 0 ,"TWC_F14_EXT_04",false};
+{"HB_F14_EXT_TAIL", 0 ,"TWC_F14_EXT_TAIL",false};
+{"HB_F14_WING_LEFT_01", 0 ,"TWC_F14_WING_LEFT",false};
+{"HB_F14_WING_RIGHT", 0 ,"TWC_F14_WING_RIGHT",false};
+{"HB_F14_LOD1_3in1", 0 ,"TWC_F14_LOD1_3in1",false};
+{"HB_F14_EXT_PILOT_HELMET", 0 ,"HB_F14_EXT_PILOT_HELMET",false};
+{"HB_F14_EXT_RIO_HELMET", 0 ,"HB_F14_EXT_PILOT_HELMET",false};
+{"HB_F14_EXT_DROPTANKS", 0 ,"TWC_F14_EXT_DROPTANK",false};
+}
+name = "TWC Low-Vis"
